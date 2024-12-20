23f413c92575377833563e7407b5656bbe8295e4
Hytalo P <is.oscar@hotmail.com>
2024-12-20 19:12:49 -0300

    Initial project upload

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
+export const ButtonContainer = styled.button<{ disabled?: boolean }>`
+  width: 100%;
+  height: 42px;
+  background-color: ${({ disabled }) => (disabled ? "#ccc" : "#81259D")};
+  color: #FFF;
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  border: 1px solid ${({ disabled }) => (disabled ? "#aaa" : "#81259D")};
+  border-radius: 21px;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: ${({ disabled }) => (disabled ? 1 : 0.6)};
+    cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  }
+`;
